497f14241d7d51c82cca99c43d5b63e08da12d0e
huyen.tran <hey.huyen@gmail.com>
2017-01-17 15:27:55 -0800

    Initial commit

---
+++ app/src/main/java/com/heyhuyen/pawrent/MainActivity.java
@@ -0,0 +1,13 @@
+package com.heyhuyen.pawrent;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
